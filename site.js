b2c38b903e06d73461bec18e2ae6e774d2111708
jordanmoonlay <jordan.leonardi@moonlay.com>
2017-07-07 16:25:20 +0700

    Add APP.GET

--- site.js
+++ site.js
@@ -3,6 +3,10 @@ var express = require('express'),
 
 app.use(express.static(__dirname + '/Public'));
 
+app.get('/index.html', function(req, res) {
+  res.send('index.html')
+})
+
 var server = app.listen(1338, function() {
   console.log('Listening on port %d', server.address().port);
 });
