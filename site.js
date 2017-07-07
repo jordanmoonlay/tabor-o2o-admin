b1e5dcad81a45588a55961fbabca87f40d3631e9
danielmoonlay <daniel.nababan@moonlay.com>
2017-07-07 14:47:07 +0700

    test azure deployment

--- site.js
+++ site.js
@@ -3,7 +3,8 @@ var express = require('express'),
 
 
 
-app.use(express.static(__dirname + '/Public'));
+// app.use(express.static(__dirname + '/Public'));
+app.use(express.static('D:/home/site/wwwroot/Public/'));
 
 var server = app.listen(1338, function() {
   console.log('Listening on port %d', server.address().port);
