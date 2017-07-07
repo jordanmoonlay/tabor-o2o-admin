85baae89141119349f1086a323eb1408d7ff280a
jordanmoonlay <jordan.leonardi@moonlay.com>
2017-07-07 16:59:27 +0700

    Modif APP.GET

--- site.js
+++ site.js
@@ -3,9 +3,11 @@ var express = require('express'),
 
 app.use(express.static(__dirname + '/Public'));
 
-app.get('/index.html', function(req, res) {
-  res.send('index.html')
+app.get('/', function(req, res) {
+  res.send('Hello World')
 })
+
+app.set('port', process.env.PORT || 1338);
 
 var server = app.listen(1338, function() {
   console.log('Listening on port %d', server.address().port);
