6b8a305907f3ee9977713ebfeb60245cb2bfceb3
danielmoonlay <daniel.nababan@moonlay.com>
2017-10-05 14:31:10 +0700

    update login & code dirapihin.

--- Public/pages/forms/user/sidebar.controller.js
+++ Public/pages/forms/user/sidebar.controller.js
@@ -1,22 +1,25 @@
 angular.module("CrudAngular")
 .controller("SidebarController",SidebarController);
 
-SidebarController.$inject = ['AuthenticationService','AuthenticationState'];
+SidebarController.$inject = ['AuthenticationService','AuthenticationState','$state','$rootScope'];
 
-function SidebarController(AuthenticationService,AuthenticationState){
+function SidebarController(AuthenticationService,AuthenticationState,$state,$rootScope){
     var main = this;
     main.Hallo = 'helloworld';
 
 (function(){
     main.authenticatedUser = AuthenticationState.getUser();
+    $rootScope.$on("$locationChangeStart", function (event, next, current) {
+        main.authenticatedUser = AuthenticationState.getUser();
+    });
 })();
 
     function logout() {
         AuthenticationService.signOut()
         AuthenticationState.remove();
 
-        $state.go('authentication.login');
+        $state.go('login');
+        main.authenticatedUser = AuthenticationState.getUser();
     }
     main.logout = logout;
-    // main.logout = logout;
 }
